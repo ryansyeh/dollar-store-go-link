0ea29f38a07db6d4d1e4e30e78c6318e34b2b7af
Ryan Yeh <ryeh@cedar.com>
2023-10-05 13:53:48 -0400

    Added basic extension boilerplate

---
+++ popup/popup.js
@@ -0,0 +1,11 @@
+// let activeTab;
+//
+// async function getCurrentTab() {
+//     chrome.tabs.query({ active: true, currentWindow: true });
+//     return tab;
+// }
+//
+// activeTab = await getCurrentTab();
+//
+// console.log({activeTab})
+console.log("LOL")
