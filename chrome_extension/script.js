11f87be98a2b86d7a535457c67be8fd64ae19852
Brian Cheang <67596730+cheangbrian@users.noreply.github.com>
2023-10-06 08:41:55 -0400

    add wizard

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -1,3 +1,5 @@
+const domain = 'http://cheangmbrian.pythonanywhere.com'
+
 async function getCurrentTab() {
     return chrome.tabs.query({ active: true, currentWindow: true});
 }
@@ -7,7 +9,42 @@ async function getCurrentUrl() {
     console.log(activeTab)
     const activeUrl = activeTab[0].url;
     console.log({activeUrl});
+
+    const res=await fetch (`${domain}/go_links`);
+    const record = await res.json();
+
+    let found_matching_go_link = false
+    let matching_go_link = ''
+
+    console.log(`Checking for ${activeUrl}`)
+    console.log(record.data)
+    for (i in record.data){
+        const d = record.data[i]
+        console.log(d)
+        if (activeUrl == d.actual_url){
+            found_matching_go_link = true
+            matching_go_link = d.go_link
+        }
+    }
+
+    if (found_matching_go_link == true){
+        success_screen.style.display="block"
+        success_title.innerText = "Found a Go!"
+        created_go_link.innerHTML += `<a href="https://${matching_go_link}" target="_blank" id="link_to_copy">https://${matching_go_link}</p>`
+        original.style.display="none"
+        document.getElementById("action-wizard").src = 'images/wizard_3.jpg'
+    }
     document.querySelector(".actual_url").value = activeUrl
+
+}
+
+async function fetchAllGoLinks() {
+    const res=await fetch (`${domain}/go_links`);
+    const record=await res.json();
+    console.log(record);
+    document.getElementById("id").innerHTML=record.data[0].id;
+    document.getElementById("actual_url").innerHTML=record.data[0].actual_url;
+    document.getElementById("go_link").innerHTML=record.data[0].go_link;
 }
 
 const go = async () => await getCurrentUrl();
@@ -18,11 +55,12 @@ const form = document.querySelector(".form-data");
 const actual_url = document.querySelector(".actual_url")
 const go_link = document.querySelector(".go_link")
 const success_screen = document.querySelector(".success")
+const success_title = document.querySelector(".success-title")
 const created_go_link = document.querySelector(".created_go_link")
 
 const createGoLink = async (actual_url, go_link) => {
     try {
-        const url = `http://cheangmbrian.pythonanywhere.com/create`;
+        const url = `${domain}/create`;
         const options = {
           method: "POST",
           headers: {
